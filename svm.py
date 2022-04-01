5cfdfe575b05d4850a419d17ab6e19391b5e8a53
Lin Wenxiang <3065746398@qq.com>
2022-04-01 12:54:44 +0800

    numpy

--- svm.py
+++ svm.py
@@ -1,11 +1,12 @@
 from sklearn import svm
+import numpy as np
 import pylab as pl
-x=[[2,0],[1,1],[2,3]]
-y=[0,0,1]
+x=np.array([[2,0],[1,1],[2,3]])
+y=np.array([0,0,1])
 clf1=svm.SVC(kernel="linear")
 clf1.fit(x,y)
 print(clf1)
 print(clf1.support_vectors_)
 print(clf1.support_)
 print(clf1.n_support_)
-print(clf1.predict([[2,0],[10,10],[0,0]]))
+print(clf1.predict(np.array([[2,0],[10,10],[0,0]])))
